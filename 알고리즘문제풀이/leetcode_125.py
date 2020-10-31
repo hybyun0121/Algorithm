bd1f85cb2da0d67b695e5cb9e045d6e75d55079a
hybyun0121 <hoyunb@gmail.com>
2020-10-31 11:53:50 +0900

    hoyoon update

---
+++ 알고리즘문제풀이/leetcode_125.py
@@ -0,0 +1,79 @@
+#[leetcode 125] : 앞뒤가 똑같은 전화번호~ Palindrome(ABB|BBA, ToDoT 이런 것들)
+
+#Hoyoon
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]','',s)
+        s_list = [i for i in s]
+
+        half_s = []
+        for i in range(len(s_list)//2):
+            half_s.append(s_list.pop())
+
+        if len(s) % 2 == 1:
+            s_list.pop()
+
+        print(len(s_list), len(half_s))
+        assert len(s_list) == len(half_s)
+
+        if half_s == s_list:
+            return True
+        else:
+            return False
+
+S = Solution()
+S.isPalindrome("0P")
+
+s = "A man, a plan, a canal: Panama"
+
+s = s.lower()
+s
+s=re.sub('[^a-zA-Z0-9]','',s)
+s
+
+
+# 민채
+
+# deque
+from collections import deque
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = deque()
+        for character in s:
+            if character.isalnum() is True:
+                strs.append(character.lower())
+            # deque[a,m,a,n,a,p,l,a,n,a,c,a,n,a,l]
+                
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop(): # O(1) + O(1)
+                return False
+        return True
+
+
+# 재호
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        flag = False
+        s = s.lower()
+        s = list(s)
+
+
+        for i in range(len(s)-1, -1, -1) :
+            if not s[i].isalnum():
+                del s[i]
+
+        if not s :
+            return True
+        elif len(s) == 1:
+            return True
+
+        for i in range(len(s)//2) :
+            if s[i] != s[len(s)-1-i] :
+                return False
+            else :
+                flag = True
+        return flag
---
+++ 알고리즘문제풀이/leetcode_561.py
@@ -0,0 +1,41 @@
+# 민채
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        # 오름차순 정렬
+        nums.sort()
+        i = 0
+        temp = 0
+        # 짝수 인덱스만 더하면 된다
+        while i <= len(nums):
+            try:
+                temp += nums[i]
+                i += 2
+            except:
+                break
+        return temp
+
+
+# 재호
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        answer = 0
+        nums = sorted(nums)
+
+        for i in range(len(nums)):
+            if i % 2 == 0 :
+                tmp = nums[i]
+            else :
+                answer = answer + min(tmp, nums[i])
+        return answer
+
+
+# 호윤
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        answer = 0
+
+        for i in range(len(nums)//2):
+            answer += nums[i*2]
+
+        return answer
