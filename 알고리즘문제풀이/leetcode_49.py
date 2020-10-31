8f0be7020f5ad66006499d659f064092e0c1bebf
jh-ubuntu <ljh415@gmail.com>
2020-10-31 14:20:09 +0900

    add jh's code

--- 알고리즘문제풀이/leetcode_49.py
+++ 알고리즘문제풀이/leetcode_49.py
@@ -20,3 +20,36 @@ class Solution:
         
         # answer = sorted(answer, key=lambda x : len(x))
         return answer
+
+# jh
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        tmp_dict = {}
+        answer = []
+        flag = [False] * len(strs)
+
+        # 딕셔너리 생성
+        for idx, str in enumerate(strs):
+            tmp_dict[idx] = [str, Counter(str)]
+
+        for i in range(0, len(tmp_dict.keys())):
+
+            if flag[i] == True :
+                continue
+            flag[i] = True
+            tmp = [tmp_dict[i][0]]
+            for j in range(i+1, len(tmp_dict.keys())):
+                if flag[j] == True :
+                    continue
+                if tmp_dict[i][1] == tmp_dict[j][1]:
+                    flag[j] = True
+                    tmp.append(tmp_dict[j][0])
+            answer.append(list(tmp))
+
+        # for i in range(len(answer)) :
+        #     answer[i] = sorted(answer[i], reverse=False)
+
+        answer = sorted(answer, key=lambda x : len(x))
+
+        return answer
